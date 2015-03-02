1c84eda7e2deaf8408a26d63d7087ab8a689d7d2
vagrant <vagrant@precise32.(none)>
2015-03-02 11:15:04 +0000

    css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,3 @@
 # application_smart_phone.cssをprecompileに加える
 Rails.application.config.assets.precompile += %w(application_smart_phone.css)
+Rails.application.config.assets.precompile += %w(users.css)
